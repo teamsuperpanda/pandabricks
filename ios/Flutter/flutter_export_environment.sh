5ea29d3cde474c85bfbdc7cffcaf8d572f3738bd
Team Super Panda <60556240+teamsuperpanda@users.noreply.github.com>
2025-08-09 14:27:52 +1200

    v2 base empty project

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\notad\Documents\Github\pandabricks"
+export "FLUTTER_ROOT=/home/howley/Dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/howley/Documents/GitHub/pandabricks/pandabricks"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.4"
-export "FLUTTER_BUILD_NUMBER=5"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
