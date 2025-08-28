f5b0471391f1306003545b2170cc2e8f594de270
Team Super Panda <60556240+teamsuperpanda@users.noreply.github.com>
2025-08-28 15:42:25 +1200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -100,3 +100,8 @@ android {
 flutter {
     source = "../.."
 }
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+}
